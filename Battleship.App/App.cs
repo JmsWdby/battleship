cf7c24942e5d3a8a5755ace1327ced54739a29a2
James Waudby <james.waudby@outlook.com>
2021-04-21 17:28:42 +0100

    Clear console window before drawing targeting grid and ntroduce a delay between player actions and outcomes

--- Battleship.App/App.cs
+++ Battleship.App/App.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Battleship.Library.Enums;
 using Battleship.Library.Exceptions;
 using Battleship.Library.Models;
@@ -86,8 +87,7 @@ namespace Battleship.App
                 _gridService.SetShipPosition(enemyGrid, enemyShip);
             }
 
-            Console.WriteLine();
-            DrawGrid(playerGrid, "Positioning");
+            DrawPositioningGrid(playerGrid);
         }
 
         private void SetUpCustomGame(Grid playerGrid, Grid enemyGrid)
@@ -119,16 +119,14 @@ namespace Battleship.App
                 }
             }
 
-            Console.WriteLine();
-            DrawGrid(playerGrid, "Positioning");
+            DrawPositioningGrid(playerGrid);
         }
 
         private void SetShipPositionManually(Grid grid, Ship ship)
         {
             try
             {
-                Console.WriteLine();
-                DrawGrid(grid, "Positioning");
+                DrawPositioningGrid(grid);
 
                 Console.WriteLine($"Ship {ship.Type}, length {ship.Length}");
 
@@ -147,6 +145,8 @@ namespace Battleship.App
         private void PlayGame(Grid playerGrid, Grid enemyGrid)
         {
             bool autoTargetShips = RequestBool("Target ships randomly:");
+            int millisecondsTimeout = RequestInt("Please enter how many seconds to wait between actions:") * 1000;
+            bool showContinuePrompt = millisecondsTimeout > 0;
 
             var rand = new Random();
             bool playersTurn = rand.NextDouble() > 0.5;
@@ -161,14 +161,11 @@ namespace Battleship.App
             {
                 int roundNumber = turnNumber / 2 + 1;
 
-                Console.WriteLine();
-                Console.WriteLine($"Round {roundNumber}");
-                Console.WriteLine($"{currentPlayer}'s turn.");
-
                 Grid targetGrid = playersTurn ? enemyGrid : playerGrid;
 
-                Console.WriteLine();
-                DrawGrid(targetGrid, "Targeting");
+                DrawTargetingGrid(targetGrid, roundNumber, currentPlayer, showContinuePrompt);
+                
+                Thread.Sleep(millisecondsTimeout);
 
                 var validTargets = _gridService.GetValidTargets(targetGrid).ToList();
 
@@ -198,8 +195,16 @@ namespace Battleship.App
 
                 Console.WriteLine();
                 Console.WriteLine($"{currentPlayer} attacks {selectedTarget}");
+                Thread.Sleep(millisecondsTimeout);
 
                 var shipType = _gridService.Attack(targetGrid, selectedTarget);
+                
+                DrawTargetingGrid(targetGrid, roundNumber, currentPlayer, showContinuePrompt);
+
+                Console.WriteLine();
+                Console.WriteLine($"{currentPlayer} attacks {selectedTarget}");
+                Console.WriteLine();
+
                 if (shipType is not null)
                 {
                     Console.WriteLine("KABOOM! Attack successful!");
@@ -212,6 +217,12 @@ namespace Battleship.App
                 else
                 {
                     Console.WriteLine("Sploosh. Attack unsuccessful.");
+                }
+
+                if (showContinuePrompt)
+                {
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadLine();
                 }
 
                 var remainingShipPositions = _gridService.GetShipPositions(targetGrid);
@@ -278,6 +289,25 @@ namespace Battleship.App
                     Console.Write(character);
                 }
             }
+        }
+
+        private static void DrawPositioningGrid(Grid grid)
+        {
+            Console.WriteLine();
+            DrawGrid(grid, "Positioning");
+        }
+
+        private static void DrawTargetingGrid(Grid grid, int roundNumber, string currentPlayer, bool clearConsole)
+        {
+            if (clearConsole)
+            {
+                Console.Clear();
+                Console.WriteLine($"Round {roundNumber}");
+                Console.WriteLine($"{currentPlayer}'s turn.");
+            }
+
+            Console.WriteLine();
+            DrawGrid(grid, "Targeting");
         }
     }
 }
